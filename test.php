03ff5534ce3615a66a61f934ac2f08cac8bf68ff
Vladislav1123 <167371964+Vladislav1123@users.noreply.github.com>
2024-05-22 00:14:10 +0300

    added.idea 7

--- test.php
+++ test.php
@@ -12,12 +12,11 @@ $personalInformation = [
     "last_name", 'lastName',
 ];
 
-while (true) {
-    $personalInformation['last_name'] = readline('Enter your last name: ');
-    if (empty($personalInformation['last_name'])) {
-        echo 'Last name cannot be empty' . PHP_EOL;
-    } else {
-        break;
-    }
-}
+reset($personalInformation);
+
+do {
+    $key = key($personalInformation);
+    $value = current($personalInformation);
+    echo $key . ' ' . $value . PHP_EOL;
+} while (next($personalInformation) !== false);
 
