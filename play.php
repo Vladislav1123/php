e5f5ae585d59b18a8e6f77d43c55be08cfd01620
Vladislav1123 <167371964+Vladislav1123@users.noreply.github.com>
2024-06-13 01:25:23 +0300

    play function For

--- play.php
+++ play.php
@@ -1,20 +1,34 @@
 <?php
 
-$myArray = array(
+$myArray = array (
     'name' => 'John Doe',
     'email' => 'john.doe@example.com',
     'age' => rand(18, 60), // Random age between 18 and 60
     'test' => 'Test field',
     'country' => 'USA',
-    'interests' => ['coding', 'music', 'sports']
+    'interests' => ['coding', 'music', 'sports'],
+    'apple' => 'MacOS',
 );
 
-
-function printWitch (array $myArray){
+/*
+function printWitchForeach (array $myArray){
     foreach ($myArray as $key => $value) {
+        if (is_array($value)) {
+            echo implode(', ', $value);
+            continue;
+        }
         echo $key . ' ' . $value . '<br>';
     }
 }
 
-printWitch($myArray);
+printWitchForeach($myArray);
+*/
 
+function printFor($myArray){
+    for ($i = 0 ; $i < array($myArray); $i++){
+        echo $myArray [$i] . '<br>';
+
+    }
+}
+
+printFor($myArray);
