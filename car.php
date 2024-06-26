7cda522b14b41a15deadae7e1ce2a5fc9225841e
Vladislav1123 <167371964+Vladislav1123@users.noreply.github.com>
2024-06-27 01:40:38 +0300

    My car types

--- car.php
+++ car.php
@@ -1,24 +1,27 @@
 <?php
 
 class car{
-    public $color;
-    public $model;
-    public $year;
+    public string $color;
+    public string $model;
+    public int $year;
+    public string $price;
 
-    public function __construct($color, $model, $year){
+
+    public function __construct( string $color, string $model, int $year, string $price ){
         $this->color = $color;
         $this->model = $model;
         $this->year = $year;
+        $this->price = $price;
     }
 
-    public function getCarInfo(){
-        return " My car " .$this->color. ' ' .$this->model. ' ' .$this->year. ' ';
+    public function getCarInfo(): string{
+        return " My car " .$this->color. ' ' .$this->model. ' ' . ' From ' .$this->year. ' priced at $ ' .$this->price ;
     }
 
 }
 
-$myCar = new Car("black", "Toyota", "2022" );
+$myCar = new Car("black", "Toyota", "2022" , '38 000.00' );
 echo $myCar->getCarInfo(), '<br>';
 
-$myCar = new Car("red", "Renault", "2005");
+$myCar = new Car("red", "Renault", "2005" , '4 200.00' );
 echo $myCar->getCarInfo(), '<br>';
