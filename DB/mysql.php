dc4765380fe8e3fd0a2a89fc8649e5c4299d9ccb
Vladislav1123 <167371964+Vladislav1123@users.noreply.github.com>
2024-07-09 00:47:58 +0300

    database connected 2

--- DB/mysql.php
+++ DB/mysql.php
@@ -17,9 +17,35 @@ echo ' connected successfully ';
 $query = 'select id, first_name, last_name from  users';
 
 
-//$result = $connection->query($query);
-//while($row = $result->fetch_assoc()){
-   // var_dump($row);
-//}
+$result = $connection->query($query);
+/*
+while($row = $result->fetch_assoc()){
+   var_dump($row);
+}
+*/
 
+global $result, $connection;
+
+$query = 'SELECT id, first_name, last_name FROM users';
+$result = $connection->query($query);
+
+
+if($result->num_rows > 0) {
+    echo '<table>';
+    echo '<tr><th>ID</th><th>First Name</th><th>Last Name</th></tr>';
+
+    while ($row = $result->fetch_assoc()) {
+        echo '<tr>';
+        echo '<td>' . $row['id'] . '</td>';
+        echo '<td>' . $row['first_name'] . '</td>';
+        echo '<td>' . $row['last_name'] . '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+}
+else {
+    echo 'No results found.';
+}
+$connection->close();
+?>
 
