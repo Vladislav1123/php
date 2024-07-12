04650ce792fe50993f60d516397ef55f0ffaebd4
Vladislav1123 <167371964+Vladislav1123@users.noreply.github.com>
2024-07-12 22:25:19 +0300

    data output

--- DB/index.php
+++ DB/index.php
@@ -22,10 +22,24 @@
 <?php
 
 require "mysql.php";
-global $result, $connection;
+$users = getUsersList();
 
 ?>
 
+<table>
+    <tr>
+        <th>ID</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+    </tr>
+    <?php foreach ($users as $user) { ?>
+        <tr>
+            <td> <?= $user['id'] ?> </td>
+            <td> <?= $user ['first_name'] ?> </td>
+            <td> <?= $user ['last_name'] ?> </td>
+        </tr>
+    <?php } ?>
+</table>
 </body>
 </html>
 
--- DB/mysql.php
+++ DB/mysql.php
@@ -1,51 +1,34 @@
 <?php
 
-$host_name = '127.0.0.1';
-$user_name = 'root';
-$password = '';
-$DB_name = 'testB';
 
-$connection = new mysqli($host_name, $user_name, $password, $DB_name);
+function createMysqlConnection(): mysqli
+{
+    $host_name = '127.0.0.1';
+    $user_name = 'root';
+    $password = '';
+    $DB_name = 'testB';
 
-if($connection->connect_error){
-    echo 'connection failed: ' .  $connection->connect_error ;
-    die();
-}
-echo ' connected successfully ';
+    $connection = new mysqli($host_name, $user_name, $password, $DB_name);
 
-
-$query = 'select id, first_name, last_name from  users';
-
-
-$result = $connection->query($query);
-/*
-while($row = $result->fetch_assoc()){
-   var_dump($row);
-}
-*/
-
-global $result, $connection;
-
-$query = 'SELECT id, first_name, last_name FROM users';
-$result = $connection->query($query);
-
-
-if($result->num_rows > 0) {
-    echo '<table>';
-    echo '<tr><th>ID</th><th>First Name</th><th>Last Name</th></tr>';
-
-    while ($row = $result->fetch_assoc()) {
-        echo '<tr>';
-        echo '<td>' . $row['id'] . '</td>';
-        echo '<td>' . $row['first_name'] . '</td>';
-        echo '<td>' . $row['last_name'] . '</td>';
-        echo '</tr>';
+    if($connection->connect_error){
+        throw new \Exception('connection failed: ' .  $connection->connect_error);
+        die();
     }
-    echo '</table>';
+
+    return $connection;
 }
-else {
-    echo 'No results found.';
+
+function getUsersList()
+{
+    $connection = createMysqlConnection();
+    $query = 'select id, first_name, last_name from  users';
+    $result = $connection->query($query);
+    $users = [];
+    if($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()){
+            $users[] = $row;
+        }
+    }
+   return $users;
 }
-$connection->close();
-?>
 
