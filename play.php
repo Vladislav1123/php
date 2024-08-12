13e3855f3a291f91e96eaa45247b5b6c4c0ae56d
Vladislav1123 <167371964+Vladislav1123@users.noreply.github.com>
2024-08-13 00:22:01 +0300

    repeat cycles 2

--- play.php
+++ play.php
@@ -1,15 +1,52 @@
 <?php
 
 $my_array = array (
-    "Last Name: " => 'Vladislav',
+    "Last Name " => 'Vladislav',
 
-    "First Name: " => 'Moiseienko',
+    "First Name " => 'Moiseienko',
 
     "Hobbies" => 'Sport'
 
 );
 
-
+/*
 foreach($my_array as $key => $value){
-    echo "$key , $value <br> ";
+    echo "$key : $value <br> ";
+}
+*/
+
+
+/*
+reset($my_array);
+
+while (key($my_array) !== null){
+    $key = key($my_array);
+    $value = current($my_array);
+
+    echo " $key - $value <br> ";
+
+    next($my_array);
+
+}
+*/
+
+
+/*
+do{ $key = key($my_array);
+    $value = current($my_array);
+
+    echo " $key : $value <br> ";
+
+    next($my_array);
+}
+while(key($my_array) !== null);
+*/
+
+$keys = array_keys($my_array);
+
+for($i = 0; $i < count($keys); $i++){
+    $key = $keys[$i];
+    $value = $my_array[$key];
+
+    echo "$key - $value <br>";
 }
